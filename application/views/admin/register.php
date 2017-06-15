594e344bebe71ebd1441ee003a249cf1232a0a16
gwenael-lesaunier <lesaunier.gwenael@gmail.com>
2017-06-15 18:00:46 +0200

    commit du soir

--- application/views/admin/register.php
+++ application/views/admin/register.php
@@ -1,3 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
+<div id="page-wrapper" >
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                     <h2>Register </h2>   
+                    </div>
+                </div>              
+                 <!-- /. ROW  -->
+                  <hr />
+              
+                 <!-- /. ROW  -->           
+    </div>
+             <!-- /. PAGE INNER  -->
+            </div>
