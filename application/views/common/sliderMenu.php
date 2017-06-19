6ba32baa80db05e14704aa6f5cd01dc5686a56ba
Corentin Monmasson <corentin.monmasson@gmail.com>
2017-06-19 10:20:39 +0200

    ajout list news

---
+++ application/views/common/sliderMenu.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!-- SLIDER -->
+<div class="row">
+  <img class="img-responsive" src="http://placehold.it/1170x500" alt="">
+</div>
