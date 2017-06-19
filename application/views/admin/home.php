a2d59e6d8322aa92dbed23f55f865bd0b71b12bd
gwenael-lesaunier <lesaunier.gwenael@gmail.com>
2017-06-19 10:20:39 +0200

    avancée admin

--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -2,16 +2,43 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <div id="page-wrapper" >
-            <div id="page-inner">
-                <div class="row">
-                    <div class="col-md-12">
-                     <h2>BLANK PAGE </h2>   
-                    </div>
-                </div>              
-                 <!-- /. ROW  -->
-                  <hr />
-              
-                 <!-- /. ROW  -->           
-    </div>
-             <!-- /. PAGE INNER  -->
+    <div id="page-inner">
+        <div class="row">
+            <div class="col-md-12">
+             <h2>ACCUEIL ADMINISTRATION</h2>   
             </div>
+        </div>              
+         <!-- /. ROW  -->
+      	<hr />
+      	<!-- contenu a partir d'ici -->
+      	<div class="row text-center pad-top">
+      		<div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+          		<div class="div-square">
+	               	<a href="blank.html" >
+						<i class="fa fa-newspaper-o fa-5x"></i>
+	          			<h4>Actualités</h4>
+	          		</a>
+          		</div>
+          	</div>
+          	<div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+          		<div class="div-square">
+	               	<a href="blank.html" >
+						<i class="fa fa-users fa-5x"></i>
+	          			<h4>Personel CCAS</h4>
+	          		</a>
+          		</div>
+          	</div>
+          	<div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+          		<div class="div-square">
+	               	<a href="blank.html" >
+						<i class="fa fa-bar-chart-o fa-5x"></i>
+	          			<h4>Statistiques</h4>
+	          		</a>
+          		</div>
+          	</div>
+      	</div>
+      	<!-- /. contenu  -->
+         <!-- /. ROW  -->           
+	</div>
+     <!-- /. PAGE INNER  -->
+</div>
