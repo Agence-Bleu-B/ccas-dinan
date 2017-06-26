8f3118fc4458f8714da72c37f58d244ac03d3009
Corentin Monmasson <corentin.monmasson@gmail.com>
2017-06-26 10:46:07 +0200

    refonte contructeur

--- application/controllers/Accueil.php
+++ application/controllers/Accueil.php
@@ -3,48 +3,42 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Accueil extends CI_Controller {
 
+	private $isco;
+	private $data = array();//data head et footer sous certaines conditions
+	private $data2 = array();//data corp
+
+	public function __construct()
+  {
+    parent::__construct();
+
+		$this->load->model('personnel_model');
+		$this->isco = $this->personnel_model->isconnect();
+		$this->data['isPersonnelCo'] = $this->isco;
+	}
 
 	public function index()
 	{
-		$data = array();
-		$data2 = array();
-
-		$this->load->model('personnel_model');
-		$data['isPersonnelCo'] = $this->personnel_model->isconnect();
-
 		$this->load->view('common/head');
-		$this->load->view('common/header', $data);
-    $this->load->view('accueil/home',$data2);
+		$this->load->view('common/header', $this->data);
+    $this->load->view('accueil/home',$this->data2);
     $this->load->view('common/footer');
 	}
 
 	public function actualites() // Liste actualites
 	{
-		$data = array();
-		$data2 = array();
-
-		$this->load->model('personnel_model');
-		$data['isPersonnelCo'] = $this->personnel_model->isconnect();
-
 		$this->load->view('common/head');
-		$this->load->view('common/header', $data);
-	    $this->load->view('accueil/actusListe',$data2);
+		$this->load->view('common/header', $this->data);
+	  $this->load->view('accueil/actusListe',$this->data2);
     $this->load->view('common/footer');
 	}
 
 	public function actualite() // Actualité unique
 	{
-		$data = array();
-		$data2 = array();
-
-		$this->load->model('personnel_model');
-		$data['isPersonnelCo'] = $this->personnel_model->isconnect();
-
 		$data['news_banner'] = true; // temporaire, à remplacé par la methode du model
 
 		$this->load->view('common/head');
-		$this->load->view('common/header', $data);
-	    $this->load->view('accueil/actu',$data2);
+		$this->load->view('common/header', $this->data);
+	  $this->load->view('accueil/actu',$this->data2);
     $this->load->view('common/footer');
 	}
 
--- application/controllers/Services.php
+++ application/controllers/Services.php
@@ -3,6 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Services extends CI_Controller {
 
+	private $isco;
+	private $data = array();//data head et footer sous certaines conditions
+	private $data2 = array();//data corp
+
+	public function __construct()
+  {
+    parent::__construct();
+
+		$this->load->model('personnel_model');
+		$this->isco = $this->personnel_model->isconnect();
+		$this->data['isPersonnelCo'] = $this->isco;
+	}
 
 	public function index()
 	{
@@ -11,67 +23,37 @@ class Services extends CI_Controller {
 
 	public function action_sociales()
 	{
-		$data = array();
-		$data2 = array();
-
-		$this->load->model('personnel_model');
-		$data['isPersonnelCo'] = $this->personnel_model->isconnect();
-
 		$this->load->view('common/head');
-		$this->load->view('common/header', $data);
-	    $this->load->view('services/actions',$data2);
+		$this->load->view('common/header', $this->data);
+	  $this->load->view('services/actions', $this->data2);
     $this->load->view('common/footer');
 	}
 	public function aides_a_domicile()
 	{
-		$data = array();
-		$data2 = array();
-
-		$this->load->model('personnel_model');
-		$data['isPersonnelCo'] = $this->personnel_model->isconnect();
-
 		$this->load->view('common/head');
-		$this->load->view('common/header', $data);
-	    $this->load->view('services/AaD',$data2);
+		$this->load->view('common/header', $this->data);
+	  $this->load->view('services/AaD', $this->data2);
     $this->load->view('common/footer');
 	}
 	public function ehpad()
 	{
-		$data = array();
-		$data2 = array();
-
-		$this->load->model('personnel_model');
-		$data['isPersonnelCo'] = $this->personnel_model->isconnect();
-
 		$this->load->view('common/head');
-		$this->load->view('common/header', $data);
-	    $this->load->view('services/ehpad',$data2);
+		$this->load->view('common/header', $this->data);
+	  $this->load->view('services/ehpad', $this->data2);
     $this->load->view('common/footer');
 	}
 	public function hebergement_durgence()
 	{
-		$data = array();
-		$data2 = array();
-
-		$this->load->model('personnel_model');
-		$data['isPersonnelCo'] = $this->personnel_model->isconnect();
-
 		$this->load->view('common/head');
-		$this->load->view('common/header', $data);
-	    $this->load->view('services/hDu',$data2);
+		$this->load->view('common/header', $this->data);
+	  $this->load->view('services/hDu', $this->data2);
     $this->load->view('common/footer');
 	}
 	public function portage_de_repas()
 	{
-		$data = array();
-		$data2 = array();
-
-		$this->load->model('personnel_model');
-		$data['isPersonnelCo'] = $this->personnel_model->isconnect();
-
 		$this->load->view('common/head');
-		$this->load->view('common/header', $data);
-	    $this->load->view('services/repas',$data2);
+		$this->load->view('common/header', $this->data);
+	  $this->load->view('services/repas', $this->data2);
     $this->load->view('common/footer');
 	}
 
