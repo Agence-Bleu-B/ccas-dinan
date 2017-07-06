8e933d8ae01e6121c737602a7cee2e48ee62d5a9
Corentin Monmasson <corentin.monmasson@gmail.com>
2017-07-06 09:23:17 +0200

    center slider

--- application/views/common/sliderMenu.php
+++ application/views/common/sliderMenu.php
@@ -4,8 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!-- SLIDER -->
 <div id="slider" class="row">
   <?php if (isset($news['couverture'])): ?> <!-- Si c'est une news (ou pasge avec banniere propre) -->
-    <img class="img-responsive" src="<?php echo img_url($news['couverture']);?>" alt="">
+    <img class="img-responsive center-block" src="<?php echo img_url($news['couverture']);?>" alt="">
   <?php else: ?> <!-- Sinon on affiche le menu slider -->
-    <img class="img-responsive" src="http://placehold.it/1170x500" alt="">
+    <img class="img-responsive center-block" src="http://placehold.it/1170x500" alt="">
   <?php endif; ?>
 </div>
