9fc6d640b619c38669bc63eafa7cd2d961339a7d
SullivanAnge <ange.sullivan@hotmail.fr>
2017-09-08 10:38:22 +0200

    up

--- application/views/accueil/doc.php
+++ application/views/accueil/doc.php
@@ -16,7 +16,86 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <section class="row">
 
     <div class="col-xs-12">
+    
+      <h2>Tous nos formulaires sont disponible ci-dessous:</h2>
+    
+     <div class="col-md-3">
+   
+       <div id="logement">
+   
+       	<div class="ruban">
+   
+       		<h4 class="logement">Aide aux logement</h4>
+           
+              <ul id="lienpdf">
+               <li><a href="#" class="pdf1l">PDF1l</a></li>
+               <li><a href="#" class="pdf2l">PDF2l</a></li>
+             </ul>
+   
+        </div>
+   
+         <div class="ruban_gauche"></div>
+   
+       </div>
+      </div>
+   
+      <div class="col-md-3">
+    
+       <div id="embauche" >
+     
+         <div class="ruban">
+    
+            <h4 class="Embauche">Aide à L'Embauche</h4>
+           
+            <ul id="lienpdf">
+              <li><a href="#" class="pdf1e">PDF1e</a></li>
+              <li><a href="#" class="pdf2e">PDF2e</a></li>
+            </ul>
+        
+        </div>
+    
+        <div class="ruban_gauche"></div>
+    
+        </div>
+      </div>
       
+      <div class="col-md-3">
+      
+       <div id="apprentissage">
+        <div class="ruban">
+       
+            <h4 calss="apprentissage">Aide à L'Apprentissage</h4>
+          
+            <ul id="lienpdf">
+              <li><a href="#" class="pdf1a">PDF1a</a></li>
+              <li><a href="#" class="pdf2a">PDF2a</a></li>
+            </ul>
+        
+        </div>
+
+        <div class="ruban_gauche"></div>
+      
+        </div>
+      </div>
+      
+      <div class="col-md-3">
+       
+       <div id="mobilité">
+        <div class="ruban">
+        
+            <h4 class="mobilité">Aide à la Mobilité</h4>
+        
+            <ul id="lienpdf">
+              <li><a href="#" class="pdf1m">PDF1m</a></li>
+              <li><a href="#" class="pdf2m">PDF2m</a></li>
+            </ul>
+         
+        </div>
+        
+        <div class="ruban_gauche"></div>
+      
+      </div>
+     </div>
     </div>
   </section>
 
